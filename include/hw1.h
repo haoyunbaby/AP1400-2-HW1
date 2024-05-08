3a090fce751978345d761b170ab3f7afc6c22f73
Xing Zhikai <haoyunbaobei1209@gmail.com>
2024-05-08 18:32:58 +0800

    Update ans
    pass all unit test

--- include/hw1.h
+++ include/hw1.h
@@ -1,5 +1,24 @@
+#include <vector>
 #ifndef AP_HW1_H
 #define AP_HW1_H
-
-
-#endif //AP_HW1_H
+namespace algebra {
+using Matrix = std::vector<std::vector<double>>;
+Matrix zeros(std::size_t n, std::size_t m);
+Matrix ones(std::size_t n, std::size_t m);
+Matrix random(std::size_t n, std::size_t m, double min, double max);
+void show(const Matrix& matrix);
+Matrix multiply(const Matrix& matrix, double c);
+Matrix multiply(const Matrix& matrix1, const Matrix& matrix2);
+Matrix sum(const Matrix& matrix, double c);
+Matrix sum(const Matrix& matrix1, const Matrix& matrix2);
+Matrix transpose(const Matrix& matrix);
+Matrix minor(const Matrix& matrix, std::size_t n, std::size_t m);
+double determinant(const Matrix& matrix);
+Matrix inverse(const Matrix& matrix);
+Matrix concatenate(const Matrix& matrix1, const Matrix& matrix2, int axis);
+Matrix ero_swap(const Matrix& matrix, std::size_t r1, std::size_t r2);
+Matrix ero_multiply(const Matrix& matrix, std::size_t r, double c);
+Matrix ero_sum(const Matrix& matrix, std::size_t r1, double c, std::size_t r2);
+Matrix upper_triangular(const Matrix& matrix);
+}  // namespace algebra
+#endif  // AP_HW1_H
